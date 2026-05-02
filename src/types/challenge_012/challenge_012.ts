8445fad0c312c41655efacfdd4d9ee02d9718ce7
maxubrq <hungtp.play@gmail.com>
2026-05-02 10:35:16 +0700

    feat: add easy types

---
+++ src/types/challenge_012/challenge_012.ts
@@ -0,0 +1,45 @@
+/* eslint-disable */
+/*
+  3057 - Push
+  -------
+  by jiangshan (@jiangshanmeta) #easy #array
+
+  ### Question
+
+  Implement the generic version of ```Array.push```
+
+  For example:
+
+  ```typescript
+  type Result = Push<[1, 2], '3'> // [1, 2, '3']
+  ```
+
+  > View on GitHub: https://tsch.js.org/3057
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Push<T extends readonly any[], U> = [...T, U];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+export type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+];
+
+export type errors = [
+  // @ts-expect-error
+  Expect<Equal<Push<number[], string>, string[]>>,
+  // @ts-expect-error
+  Expect<Equal<Push<string[], number>, [string, number]>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3057/answer
+  > View solutions: https://tsch.js.org/3057/solutions
+  > More Challenges: https://tsch.js.org
+*/
